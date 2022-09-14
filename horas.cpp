c54c954fd74cc6eb295f702f5038c659a282e066
Ariane Sousa <111510431+arianesof@users.noreply.github.com>
2022-09-14 17:19:59 -0300

    Add files via upload

--- horas.cpp
+++ horas.cpp
@@ -2,18 +2,23 @@
 int main(){
 	int hora[10], minuto[10], segundo[10];
 	int i;
-	char nomeUsuario[50]; //%s scanf("%s", &nomeUsuario)
-
-	for (i=0; i<3; i++){ //i = 0
-		printf("Digite o valor da hora: "); // 1
-		scanf("%i", &hora[i]); // hora[0] = 1
-		minuto[i] = hora[i] * 60; // minuto[0] = 1 * 60 = 60
-		segundo[i] = minuto[i] * 60;//segundo [0] = 3600
+	char nomeUsuario[50]; 
+		for (i=0; i<1; i++){ 
+		printf("digite nome de Usuario: ");
+		scanf("%s", &nomeUsuario[i]);
+		
+	}
+	
+	for (i=0; i<1; i++){ 
+		printf("Digite o valor da hora: "); 
+		scanf("%i", &hora[i]); 
+		minuto[i] = hora[i] * 60; 
+		segundo[i] = minuto[i] * 60;
 		printf("Horas: %d\n", hora[i]);
 		printf("Minutos: %d\n", minuto[i]);
 		printf("Segundos: %d\n", segundo[i]);
 	}
 
 
-
 }
+
